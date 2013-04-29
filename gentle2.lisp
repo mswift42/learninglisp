052ac006f799cc07d1e420dd2366e0577413b2ab
mswift42 <martin.haesler@gmail.com>
2013-04-29 13:21:22 +0100

    More Solutions for Chapter 8.

--- gentle2.lisp
+++ gentle2.lisp
@@ -106,3 +106,66 @@
     ((> n (length list)) nil)
     ((zerop n) (first list))
     (t (my-nth-modified (1- n) (rest list)))))
+
+(setf family
+      '((colin nil nil)
+	(deirdre nil nil)
+	(arthur nil nil)
+	(kate nil nil)
+	(frank nil nil)
+	(linda nil nil)
+	(suzanne colin deirdre)
+	(bruce arthur kate)
+	(charles arthur kate)
+	(david arthur kate)
+	(ellen arthur kate)
+	(george frank linda)
+	(hillary frank linda)
+	(andre nil nil)
+	(tamara bruce suzanne)
+	(vincent bruce suzanne)
+	(wanda nil nil)
+	(ivan george ellen)
+	(julie george ellen)
+	(marie george ellen)
+	(nigel andre hillary)
+	(frederick nil tamara)
+	(zelda vincent wanda)
+	(joshua ivan wanda)
+	(quentin nil nil)
+	(robert quentin julie)
+	(olivia nigel marie)
+	(peter nigel marie)
+	(erica nil nil)
+	(yvette robert zelda)
+	(diane peter erica)))
+
+(defun father (name)
+  (second (assoc name family)))
+
+(defun mother (name)
+  (third (assoc name family)))
+
+(defun parents (name)
+  (rest (assoc name family)))
+
+(defun children (name)
+  (mapcar #'first ( remove-if-not #'(lambda (x)
+				      (or (equal (second x) name)
+					  (equal (third x)  name))) family)))
+
+
+(defun siblings (name)
+  (remove name (union (children (father name)) (children (mother name)))))
+
+(defun mapunion (fn list)
+  (reduce #'union (mapcar fn list)))
+
+(defun grandparents (name)
+  (mapunion #'parents (parents name)))
+
+(defun cousins (name)
+  (mapunion #'children (mapunion #'siblings (parents name))))
+
+
+
