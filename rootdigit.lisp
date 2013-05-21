82bac2898d28afc737d01a2f715a86aaae8e6677
mswift42 <martin.haesler@gmail.com>
2013-05-21 20:54:41 +0100

    Corrected typo.

--- rootdigit.lisp
+++ rootdigit.lisp
@@ -14,19 +14,27 @@
 (lisp-unit:define-test test-root-digits
   (lisp-unit:assert-equal 8 (root-digits '(3 1 3 3 7))))
 
+(lisp-unit:define-test test-result
+  (lisp-unit:assert-equal 1 (result 1073741824)))
+
 
 (defun number-to-list (nr)
+  "Convert number to list of its digits."
   (map 'list #'digit-char-p (prin1-to-string nr)))
 
 (defun sum-list (lst)
+  "Sum numbers in a list"
   (reduce #'+ lst))
 
 (defun root-digits (lst)
+  "Recursively get the root digit of a number."
   (cond
     ((null lst) 0)
     ((< (sum-list lst) 10) (sum-list lst))
-    (t (sum-list (number-to-list (sum-list lst))))))
+    (t (root-digits (number-to-list (sum-list lst)))))) 
 
+(defun result (nr)
+  (root-digits (number-to-list nr)))
 
 
 
