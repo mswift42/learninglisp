d8baf5765474f7e9b4f09ab08a86dd5f5ed1abcf
mswift42 <martin.haesler@gmail.com>
2013-06-23 14:25:41 +0100

    First working framework.

---
+++ Scores.lisp
@@ -0,0 +1,77 @@
+;;; Third Implementation of livescores app. This time in CL.
+
+(load "~/quicklisp/setup.lisp")
+(ql:quickload "ltk")
+(ql:quickload "drakma")
+(ql:quickload "rss")
+
+(defpackage :myed
+  (:use :ltk :cl :drakma ))
+
+(in-package :myed)
+
+(defparameter *leagues* '( ( bundesliga "http://rss.kicker.de/live/bundesliga") ( 2-Bundesliga "http://rss.kicker.de/live/2bundesliga") ( champions-league "http://rss.kicker.de/live/championsleague") ( DFB-Pokal "http://rss.kicker.de/live/dfbpokal") ( Premier-League "http://rss.kicker.de/live/premierleague") ( England-Championship "http://rss.kicker.de/live/thecocacolafootballleaguechampionship") ( France "http://rss.kicker.de/live/thecocacolafootballleaguechampionship") ( Spain "http://rss.kicker.de/live/primeradivision") ( Kicker-News "http://rss.kicker.de/news/aktuell") ( Scotland "http://rss.kicker.de/live/schottland") ( Switzerland "http://rss.kicker.de/live/axposuperleague") ( Netherlands "http://rss.kicker.de/live/eredivisie")))
+
+(defun feedadress (league)
+  "Return string of feed-url"
+  (first (rest (assoc league *leagues*))))
+
+
+(defun feedurl (league)
+  "download the feedpage in a rss:parse-able format"
+  (http-request (feedadress league) ))
+
+
+(defun feedstr (target league)
+  (loop for i in (both-feed league)
+        do (append-text target (format nil "~%~A" i))))
+
+
+(defun title-feed (league)
+  "Return rss:title of feed"
+  (map 'list  #'(lambda (x) (rss:title x)) (rss:items (rss:parse-rss-stream (feedurl league)))))
+
+
+(defun description-feed (league)
+  "Return description of feed"
+  (map 'list #'(lambda (x) (rss:description x)) (rss:items (rss:parse-rss-stream (feedurl league)))))
+
+(defun both-feed (league)
+  (mapcar #'list (title-feed league) (description-feed league)))
+
+(defun score-label (fr)
+  (make-instance 'label :master fr :font "Sans-Serif" :text "Scores"))
+
+
+(defun mainwindow ()
+  (with-ltk ()
+    (let* ((f (make-instance 'frame))
+	   (g (make-instance 'frame))
+	   (scroll (make-instance 'scrolled-text :master g))
+	   (textwin (textbox scroll))
+	   (lb (make-instance 'listbox)))
+      (pack f)
+      (pack g :expand :t :fill :both :ipady 2)
+      (configure textwin :font "monospaced" :background "#aea79f" :wrap :word)
+      (pack (score-label f))
+      (listbox-append lb (mapcar #'first *leagues*))
+      (pack lb  :side :bottom :anchor :sw  :fill :x)
+      (pack scroll  :expand :t :ipady 2 :fill :both)
+      
+      ;; (bind lb "<<ListboxSelect>>" (lambda (event)
+      ;; 				     (feedstr textwin
+      ;; 					      g					      (first (nth (first (listbox-get-selection lb)) *leagues*)))))
+      (bind lb "<<ListboxSelect>>" (lambda (event) (update-view textwin lb)))
+      (configure f :borderwidth 4)
+      (feedstr textwin 'spain))))
+
+
+(defun update-view (target source)
+  (clear-text target)
+  (feedstr target (first (nth (first (listbox-get-selection source)) *leagues*))))
+
+
+
+
+
+
