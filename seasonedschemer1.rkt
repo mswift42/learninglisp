4aee7003980347d18a3a480e4e8668f277bc7753
mswift42 <martin.haesler@gmail.com>
2013-10-10 17:12:37 +0100

    Implemented multirember.

--- seasonedschemer1.rkt
+++ seasonedschemer1.rkt
@@ -51,3 +51,41 @@
 
 (define (sum-of-prefixes lst)
   (sum-of-prefixes-b 0 lst))
+
+
+(define (pick n lst)
+  "retrun n-th element from lst counting from 1"
+  (if (= 1 n)
+      (first lst)
+      (pick (sub1 n) (cdr lst))))
+
+(define (scrable-b lst rev-pre)
+  (if (null? lst) '()
+      (cons (pick (first lst)
+		  (cons (first lst) rev-pre))
+	    (scrable-b (cdr lst)
+		       (cons (first lst) rev-pre)))))
+
+(define (scramble lst)
+  (scrable-b lst '()))
+
+(define (my-multirember a lst)
+  "remove all occurrences of a in lst"
+  (cond
+   ((null? lst) '())
+   ((eq? a (first lst))
+    (my-multirember a (cdr lst)))
+   (else (cons (first lst) (my-multirember a (cdr lst))))))
+
+(define (multirember a lst)
+  (letrec ((mr (lambda (lst)
+		 (cond
+		  ((null? lst) '())
+		  ((eq? a (first lst))
+		   (mr (cdr lst)))
+		  (else
+		   (cons (first lst)
+			 (mr (cdr lst))))))))
+    (mr lst)))
+
+
